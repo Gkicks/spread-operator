c0f637fe7a6e8c5ee827095e4756fc2a4d9d2e56
Gkicks <>
2023-11-04 13:00:39 +0000

    finish section

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -4,9 +4,41 @@
  */
 
 // No spread operator
+let arr1 = [1, 2, 3]
+let arr2 = arr1
+arr2.push(4)
+console.log("second array:", arr2);
 
 // Copying an array
 
+let arr3 = [4, 5, 6];
+let arr4 = [...arr3];
+console.log("fourth array:", arr4);
+arr4.push(7);
+console.log("fourth array:", arr4);
+console.log("third array:", arr3);
+
+
 // Copying an object
 
+let obj1 = {a: 1, b: 2, c: 3}
+let obj2 = {...obj1, d: 4}
+
+console.log("first object:", obj1)
+console.log("second object:", obj2)
+
+let obj3 = {...obj1, b: 5}
+
+console.log("third object:", obj3)
+
+obj1 = {a: 7}
+
+console.log("first object:", obj1)
+console.log("second object:", obj2)
+
+
+
 // Copying only part of an array/object
+
+let arr5 = [...arr1, {...obj2}, ...arr3, 8, 9, "a"]
+console.log("fifth objectarray:", arr5)
